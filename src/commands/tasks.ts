ec2205fb21b90b358c66aad23c807d27f8249f7c
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-24 15:47:07 +0530

    added a table generator

--- src/commands/tasks.ts
+++ src/commands/tasks.ts
@@ -1,5 +1,10 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+import { generateTable } from "../utils/tables.ts";
+
+const action = () => {
+  generateTable();
+};
 
 export const tasks = new Command()
   .description("Display all the existings tasks")
-  .action(() => console.log("tasks"))
+  .action(action);
