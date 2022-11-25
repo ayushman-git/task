1d84d807d8e867ccc53b5b543dbf33a4e790ced0
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-25 14:33:33 +0530

    added action handler

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,5 +1,13 @@
-import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+import {
+  ActionHandler,
+  Command,
+} from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+
+const action: ActionHandler = (_, message: string) => {
+  console.log(message);
+};
 
 export const create = new Command()
+  .arguments("<message>")
   .description("Create a new task")
-  .action(() => console.log("create"))
+  .action(action);
