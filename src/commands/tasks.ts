f78fab4a774fe77c507aac65f7bea392f871499c
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-25 15:02:23 +0530

    populating tasks on table with tasks command

--- src/commands/tasks.ts
+++ src/commands/tasks.ts
@@ -1,8 +1,20 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 import { generateTable } from "../utils/tables.ts";
 
+const getTasks = () => {
+  const db = new DB("src/db/main.db");
+
+  const tasks = db.query("SELECT * from tasks");
+  db.close();
+  return tasks;
+};
+
 const action = () => {
-  generateTable();
+  const headers = ["ID", "Title", "Status", "Priority", "Created", "Due"];
+  const body = getTasks();
+
+  generateTable(headers, body);
 };
 
 export const tasks = new Command()
--- src/utils/tables.ts
+++ src/utils/tables.ts
@@ -1,17 +1,12 @@
 import { Table } from "https://deno.land/x/cliffy@v0.25.4/table/mod.ts";
 
-export const generateTable = () => {
+export const generateTable = (headers: string[], body: Array<string[]>) => {
   new Table()
-  .header(["Name", "Date", "City", "Country"])
-  .body([
-    ["Baxter Herman", "Oct 1, 2020", "Harderwijk", "Slovenia"],
-    ["Jescie Wolfe", "Dec 4, 2020", "Alto Hospicio", "Japan"],
-    ["Allegra Cleveland", "Apr 16, 2020", "Avernas-le-Bauduin", "Samoa"],
-    ["Aretha Gamble", "Feb 22, 2021", "Honolulu", "Georgia"],
-  ])
-  .maxColWidth(10)
-  .padding(1)
-  .indent(2)
-  .border(true)
-  .render();
+    .header(headers)
+    .body(body)
+    .maxColWidth(10)
+    .padding(1)
+    .indent(2)
+    .border(true)
+    .render();
 };
