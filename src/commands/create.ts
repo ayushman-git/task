c28c09e5ed68e09d305de0fec256bdc8c4aff914
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-26 12:37:21 +0530

    added feedback for creating task

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,17 +1,29 @@
+import { TaskStatus } from "../types/index.ts";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
 import {
   ActionHandler,
   Command,
 } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+import { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/colors.ts";
 
-const action: ActionHandler = (_, message: string) => {
+const action: ActionHandler = ({ status }, message: string) => {
   const db = new DB("src/db/main.db");
 
-  db.query("INSERT INTO tasks (title) VALUES (?)", [message]);
+  const test = db.query("INSERT INTO tasks (title, status) VALUES (?, ?)", [
+    message,
+    status,
+  ]);
+  console.log(test)
   db.close();
+  console.log(colors.green("Task created successfully ✔"))
 };
 
 export const create = new Command()
   .arguments("<message:string>")
+  .complete(
+    "status",
+    () => [TaskStatus.TODO, TaskStatus.DOING, TaskStatus.DOING],
+  )
+  .option("-s, --status <status:string:status>", "Define status of task.")
   .description("Create a new task")
   .action(action);
