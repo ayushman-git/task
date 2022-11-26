c2bf014f0568a2ed48173d3d2433089b1e88ce3f
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-26 13:00:01 +0530

    typo fix

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -8,14 +8,7 @@ import { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/colors.ts";
 
 const action: ActionHandler = ({ status }, message: string) => {
   const db = new DB("src/db/main.db");
-  if (!statusCheck(status)) {
-    console.log(
-      "❌",
-      colors.red("Accpected status values are"),
-      colors.red.bold("todo, doing and done."),
-    );
-    return;
-  }
+  if (!statusCheck(status)) return;
 
   db.query("INSERT INTO tasks (title, status) VALUES (?, ?)", [
     message,
@@ -27,7 +20,15 @@ const action: ActionHandler = ({ status }, message: string) => {
 };
 
 const statusCheck = (status: string) => {
-  return Object.values(TaskStatus).includes(status as TaskStatus);
+  const isCorrect = Object.values(TaskStatus).includes(status as TaskStatus);
+  if (!isCorrect) {
+    console.log(
+      "❌",
+      colors.red("Accepted status values are"),
+      colors.red.bold("todo, doing and done."),
+    );
+  }
+  return isCorrect;
 };
 
 export const create = new Command()
