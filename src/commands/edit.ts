00f5b003778112db63d896ca6a3bf13287332a56
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-26 20:29:32 +0530

    added all the flags

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -1,11 +1,75 @@
+import { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/colors.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { TaskPriority, TaskStatus } from "../types/index.ts";
 
+const generateQuery = ({ title, status, priority }: any, tid: number) => {
+  const queriesArr: string[] = [];
 
-const action = (_, tid: number) => {
-  
+  if (title) {
+    queriesArr.push(`title = '${title}'`);
+  }
+  if (status) {
+    queriesArr.push(`status = '${status}'`);
+  }
+  if (priority) {
+    queriesArr.push(`priority = '${priority}'`);
+  }
+  const setQuery = queriesArr.reduce((acc, current, currentIndex) => {
+    return `${acc} ${current}${
+      currentIndex === queriesArr.length - 1 ? "" : ","
+    }`;
+  }, "");
+  return setQuery ? `UPDATE tasks SET ${setQuery} WHERE id = ${tid}` : "";
+};
+
+const action = ({ title, status, priority }: any, tid: number) => {
+  const db = new DB("src/db/main.db");
+
+  try {
+    const queryStr = generateQuery({ title, status, priority }, tid);
+    console.log(queryStr);
+    if (queryStr) {
+      db.query(queryStr);
+    } else {
+      console.log(colors.red("At least one flag is needed to edit!"));
+      return;
+    }
+  } catch (_) {
+    console.log(colors.red.bold("Some internal error has occured!"));
+  } finally {
+    db.close();
+  }
+
+  console.log(
+    colors.green("✔ Task ID"),
+    colors.green.bold(tid.toString()),
+    colors.green("successfully edited!"),
+  );
 };
 
 export const edit = new Command()
-.arguments("<tid:number>")
+  .alias("ed")
+  .arguments("<tid:number>")
+  .option(
+    "-t, --title <title:string:title>",
+    "Replaces the title.",
+  )
+  .complete(
+    "status",
+    () => [TaskStatus.TODO, TaskStatus.DOING, TaskStatus.DOING],
+  )
+  .complete(
+    "priority",
+    () => [TaskPriority.LOW, TaskPriority.NORMAL, TaskPriority.HIGH],
+  )
+  .option(
+    "-s, --status <status:string:status>",
+    "Replaces the status.",
+  )
+  .option(
+    "-p, --priority <priority:string:priority>",
+    "Define priority of task.",
+  )
   .description("Edit a task")
   .action(action);
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
 import { create } from "./create.ts";
+import { edit } from "./edit.ts";
 import { remove } from "./remove.ts";
 import { tasks } from "./tasks.ts";
 
@@ -9,6 +10,7 @@ const commands = await new Command()
   .command("create", create)
   .command("tasks", tasks)
   .command("remove", remove)
+  .command("edit", edit)
   .parse(Deno.args);
 
 export const setupCommands = () => {
