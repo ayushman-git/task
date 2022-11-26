6cf36793f370cec6740ed19609ac7f63f5d05019
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-26 13:10:41 +0530

    added -p option, made logs dynamic

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,4 +1,4 @@
-import { TaskStatus } from "../types/index.ts";
+import { TaskPriority, TaskStatus } from "../types/index.ts";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
 import {
   ActionHandler,
@@ -6,17 +6,18 @@ import {
 } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
 import { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/colors.ts";
 
-const action: ActionHandler = ({ status }, message: string) => {
+const action: ActionHandler = ({ status, priority }, message: string) => {
   const db = new DB("src/db/main.db");
-  if (!statusCheck(status)) return;
+  if (status && !statusCheck(status)) return;
 
-  db.query("INSERT INTO tasks (title, status) VALUES (?, ?)", [
+  db.query("INSERT INTO tasks (title, status, priority) VALUES (?, ?, ?)", [
     message,
-    status,
+    status || TaskStatus.TODO,
+    priority || TaskPriority.NORMAL,
   ]);
 
   db.close();
-  console.log(colors.green("Task created successfully ✔"));
+  console.log(colors.green("✔ Task successfully created! "));
 };
 
 const statusCheck = (status: string) => {
@@ -25,7 +26,9 @@ const statusCheck = (status: string) => {
     console.log(
       "❌",
       colors.red("Accepted status values are"),
-      colors.red.bold("todo, doing and done."),
+      colors.red.bold(
+        `${TaskStatus.TODO}, ${TaskStatus.DOING} and ${TaskStatus.DOING}.`,
+      ),
     );
   }
   return isCorrect;
@@ -37,6 +40,14 @@ export const create = new Command()
     "status",
     () => [TaskStatus.TODO, TaskStatus.DOING, TaskStatus.DOING],
   )
+  .complete(
+    "priority",
+    () => [TaskPriority.LOW, TaskPriority.NORMAL, TaskPriority.HIGH],
+  )
   .option("-s, --status <status:string:status>", "Define status of task.")
+  .option(
+    "-p, --priority <priority:string:priority>",
+    "Define priority of task.",
+  )
   .description("Create a new task")
   .action(action);
