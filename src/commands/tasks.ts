b9c2f3ce1d2b85f46104d21d2c107be743e25e2e
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-11-28 19:03:06 +0530

    displaying based on attribute of task

--- src/commands/tasks.ts
+++ src/commands/tasks.ts
@@ -1,12 +1,33 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
 import { DB, Row } from "https://deno.land/x/sqlite/mod.ts";
+import { TaskPriority, TaskStatus } from "../types/index.ts";
 import { convertToReadableDueAndCreated } from "../utils/datetime.ts";
 import { generateTable } from "../utils/tables.ts";
 
-const getTasks = () => {
+const generateQuery = ({ status, priority }: any) => {
+  const queriesArr: string[] = [];
+
+  if (status) {
+    queriesArr.push(`status = '${status}'`);
+  }
+  if (priority) {
+    queriesArr.push(`priority = '${priority}'`);
+  }
+  const setQuery = queriesArr.reduce((acc, current, currentIndex) => {
+    return `${acc} ${current}${
+      currentIndex === queriesArr.length - 1 ? "" : " AND"
+    }`;
+  }, " WHERE");
+  console.log(setQuery);
+  return setQuery
+    ? `SELECT * from tasks ${setQuery} ORDER BY created_at DESC`
+    : "";
+};
+
+const getTasks = ({ priority, status }: any) => {
   const db = new DB("src/db/main.db");
 
-  const tasks = db.query("SELECT * from tasks ORDER BY created_at DESC");
+  const tasks = db.query(generateQuery({ status, priority }));
   db.close();
   return tasks;
 };
@@ -24,12 +45,28 @@ const covertDatetime = (data: Row[]): any[] => {
   });
 };
 
-const action = () => {
+const action = ({ priority, status }: any) => {
   const headers = ["ID", "Title", "Status", "Priority", "Created", "Due"];
-  const body = getTasks();
+  const body = getTasks({ priority, status });
   generateTable(headers, covertDatetime(body));
 };
 
 export const tasks = new Command()
+  .complete(
+    "priority",
+    () => [TaskPriority.LOW, TaskPriority.NORMAL, TaskPriority.HIGH],
+  )
+  .option(
+    "-p, --priority <priority:string:priority>",
+    "Displays tasks based on priority.",
+  )
+  .complete(
+    "status",
+    () => [TaskStatus.TODO, TaskStatus.DOING, TaskStatus.DONE],
+  )
+  .option(
+    "-s, --status <status:string:status>",
+    "Displays tasks based on status.",
+  )
   .description("Display all the existings tasks")
   .action(action);
